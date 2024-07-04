aff7b21910fad3743830eea114ddc72259820bf1
Daulet Abdrakhmanov <daulet@MacBook-Air-Daulet.local>
2024-07-05 00:02:39 +0500

    hard code

--- lib/presentation/_widgets/news_item.dart
+++ lib/presentation/_widgets/news_item.dart
@@ -1,11 +1,49 @@
-import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:get_it/get_it.dart';
 import 'package:news/domain/entities/export_entities.dart';
+import 'package:news/domain/repositories/export_repositories.dart';
 
-class NewsItem extends StatelessWidget {
-  const NewsItem({super.key, required this.news});
+class NewsItem extends StatefulWidget {
+  const NewsItem({
+    super.key,
+    required this.news,
+  });
 
   final NewsEntity news;
+
+  @override
+  State<NewsItem> createState() => _NewsItemState();
+}
+
+class _NewsItemState extends State<NewsItem> {
+  final _newsRepo = GetIt.instance<NewsRepository>();
+  bool isLiked = false;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _checkIsLiked(widget.news.url);
+  }
+
+  void _onLikePressed(String? url) {
+    if (url == null) return;
+
+    isLiked ? _newsRepo.unlikeNews(url: url) : _newsRepo.likeNews(url: url);
+    setState(() => isLiked = !isLiked);
+  }
+
+  void _checkIsLiked(String? url) async {
+    if (url == null) {
+      isLiked = false;
+      return;
+    }
+
+    isLiked = await _newsRepo.isNewsLiked(url: url);
+    setState(() {
+      isLiked;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -27,34 +65,32 @@ class NewsItem extends StatelessWidget {
                   image: AssetImage("assets/images/no_image.png"),
                 ),
               ),
-              child: news.urlToImage != null
+              child: widget.news.urlToImage != null
                   ? ClipRRect(
                       borderRadius: BorderRadius.circular(8),
                       child: Image.network(
-                        news.urlToImage!,
+                        widget.news.urlToImage!,
                         fit: BoxFit.cover,
                       ),
                     )
                   : null,
-              // child: ClipRRect(
-              //   borderRadius: BorderRadius.circular(8),
-              //   child: AspectRatio(
-              //     aspectRatio: 1,
-              //     child: Image.network(
-              //       news.urlToImage,
-              //       fit: BoxFit.cover,
-              //       loadingBuilder: (context, child, loadingProgress) {
-              //         if (loadingProgress == null) return child;
-              //         return const Center(child: CircularProgressIndicator());
-              //       },
-              //     ),
-              //   ),
-              // ),
             ),
             const SizedBox(height: 6),
-            Text(news.title ?? "Новость"),
+            Text(widget.news.title ?? "Новость"),
             const SizedBox(height: 6),
-            Text(news.description ?? news.content ?? ""),
+            Text(widget.news.description ?? widget.news.content ?? ""),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                IconButton(
+                  onPressed: () => _onLikePressed(widget.news.url),
+                  icon: Icon(
+                    Icons.favorite,
+                    color: isLiked ? Colors.red : null,
+                  ),
+                ),
+              ],
+            )
           ],
         ),
       ),
